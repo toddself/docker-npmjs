51bd0223687f7aa31f69a53ed74fb69bca7c6d5b
Todd Kennedy <todd@selfassembled.org>
2014-05-29 14:02:15 -0400

    adding volume support to config...

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -14,8 +14,8 @@ if [ ! -f $VOLUME/.initialized ]; then
   touch $VOLUME/.initialized
 fi;
 
-cat /usr/local/etc/couchdb/default.ini | sed -e "s/database_dir = .*/database_dir = $VOLUME/" > /root/default.ini
-cp /root/default.ini /usr/local/etc/default.ini
-echo -e "[vhosts]\n$FULLHOST:5984 = /registry/_design/app/_rewrite" >> /usr/local/etc/couchdb/local.d/npmjs-vhost.ini
+echo -e "[couchdb]\ndatabase_dir = $VOLUME\nview_index_dir = $VOLUME" > /usr/local/etc/couchdb/local.d/couchdb.ini
+echo -e "[log]\nfile = $VOLUME/couch.log" > /usr/local/etc/local.d/log.ini
+echo -e "[vhosts]\n$FULLHOST:5984 = /registry/_design/app/_rewrite" > /usr/local/etc/couchdb/local.d/npmjs-vhost.ini
 cat /opt/npmjs/kappa.json.default | sed -e "s/\${hostname}/$FULLHOST/" | sed -e "s/\${payload}/$PAYLOAD/" > /opt/npmjs/kappa.json
-couchdb -b; kappa -c /opt/npmjs/kappa.json & tail -f /usr/local/var/log/couchdb/couch.log
+couchdb -b; kappa -c /opt/npmjs/kappa.json & tail -f $VOLUME/couch.log
