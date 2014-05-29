67d91e686bd933eb7628e72186ab6a095f96325a
Todd Kennedy <todd@selfassembled.org>
2014-05-29 13:41:36 -0400

    path

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -12,7 +12,7 @@ fi
 if [ ! -f $VOLUME/.initialized ]; then
   cp -R /usr/local/var/lib/couchdb/* $VOLUME/.;
 fi;
-cat /usr/local/etc/default.ini | sed -e "s/database_dir = .*/database_dir = $VOLUME/" > /root/default.ini
+cat /usr/local/etc/couchdb/default.ini | sed -e "s/database_dir = .*/database_dir = $VOLUME/" > /root/default.ini
 cp /root/default.ini /usr/local/etc/default.ini
 echo -e "[vhosts]\n$FULLHOST:5984 = /registry/_design/app/_rewrite" >> /usr/local/etc/couchdb/local.d/npmjs-vhost.ini
 cat /opt/npmjs/kappa.json.default | sed -e "s/\${hostname}/$FULLHOST/" | sed -e "s/\${payload}/$PAYLOAD/" > /opt/npmjs/kappa.json
