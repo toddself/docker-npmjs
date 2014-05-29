ba3b38f765ee45765882d3bd99156b75371a60ca
Todd Kennedy <todd@selfassembled.org>
2014-05-29 14:53:28 -0400

    finicky

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -11,11 +11,11 @@ fi
 
 if [ ! -f $VOLUME/.initialized ]; then
   cp -R /usr/local/var/lib/couchdb/* $VOLUME/.;
+  echo -e "[couchdb]\ndatabase_dir = $VOLUME\nview_index_dir = $VOLUME" > /usr/local/etc/couchdb/local.d/couchdb.ini
+  echo -e "[log]\nfile = $VOLUME/couch.log" > /usr/local/etc/couchdb/local.d/log.ini
+  echo -e "[vhosts]\n$FULLHOST:5984 = /registry/_design/app/_rewrite" > /usr/local/etc/couchdb/local.d/npmjs-vhost.ini
+  cat /opt/npmjs/kappa.json.default | sed -e "s/\${hostname}/$FULLHOST/" | sed -e "s/\${payload}/$PAYLOAD/" > /var/lib/couchdb/kappa.json
   touch $VOLUME/.initialized
 fi;
 
-echo -e "[couchdb]\ndatabase_dir = $VOLUME\nview_index_dir = $VOLUME" > /usr/local/etc/couchdb/local.d/couchdb.ini
-echo -e "[log]\nfile = $VOLUME/couch.log" > /usr/local/etc/couchdb/local.d/log.ini
-echo -e "[vhosts]\n$FULLHOST:5984 = /registry/_design/app/_rewrite" > /usr/local/etc/couchdb/local.d/npmjs-vhost.ini
-cat /opt/npmjs/kappa.json.default | sed -e "s/\${hostname}/$FULLHOST/" | sed -e "s/\${payload}/$PAYLOAD/" > /var/lib/couchdb/kappa.json
 couchdb -b; kappa -c /var/lib/couchdb/kappa.json & tail -f $VOLUME/couch.log
